17c921206d54f5619613909e81e7f27485a36934
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-01-07 07:27:15 +0800

    add seed

--- RichillCapital.Infrastructure/Persistence/Seeds.cs
+++ RichillCapital.Infrastructure/Persistence/Seeds.cs
@@ -1,12 +1,22 @@
 using Microsoft.Extensions.DependencyInjection;
 
+using RichillCapital.Core.Domain.Entities;
+using RichillCapital.Core.Domain.ValueObjects;
+
 namespace RichillCapital.Infrastructure.Persistence;
 
 public static class Seeds
 {
+    private static readonly List<User> Users = [
+        User.Create(new UserId("1"), new Email("mengsyue.tsai@outlook.com"), new Name("Meng Syue Tsai")),
+        User.Create(new UserId("2"), new Email("mengsyue.tsai@gmail.com"), new Name("Meng Syue Tsai")),
+    ];
+
     public static void Initialize(IServiceProvider serviceProvider)
     {
         using var context = serviceProvider.GetRequiredService<MsSqlDbContext>();
+
+        context.Set<User>().AddRange(Users);
 
         context.SaveChanges();
     }
